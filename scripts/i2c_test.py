d8f877925726708d2e35ec583d19122421f29892
Arty <Aatharvakawade@gmail.com>
2025-06-12 21:36:16 +0200

    updating the scripts

--- scripts/i2c_test.py
+++ scripts/i2c_test.py
@@ -1,21 +1,26 @@
 import ssd1306
-import tcs34725
+#import tcs34725
 import time
-from machine import I2C, Pin
+from machine import I2C, Pin, ADC
 
 # Define I2C
 i2c = I2C(scl=Pin(22), sda=Pin(23), freq=100000)
 
-# Define olef
+# Define oled
 oled = ssd1306.SSD1306_I2C(128, 64, i2c)
 oled.fill(0)
 
 # Define rgb sensor
-sensor = tcs34725.TCS34725(i2c)
+#sensor = tcs34725.TCS34725(i2c)
 #sensor.integration_time(10) #value between 2.4 and 614.4.
 #sensor.gain(16) #must be a value of 1, 4, 16, 60
 
-def color_rgb_bytes(color_raw):
+#Defining the phototransistor sensor
+photo_adc = ADC(Pin(32, Pin.PULL_DOWN))
+photo_adc.atten(ADC.ATTN_11DB)  
+  
+
+def color_rgb_bytes(color_raw): #This code is only used for rgb sensor
     """Read the RGB color detected by the sensor.  Returns a 3-tuple of
     red, green, blue component values as bytes (0-255).
     NOTE: These values are normalized against 'clear', remove the division
@@ -39,18 +44,25 @@ def color_rgb_bytes(color_raw):
 
 while True:
     # Read color sensor
-    r,g,b = color_rgb_bytes(sensor.read(True))
+    #r,g,b = color_rgb_bytes(sensor.read(True))
+    # Read the OD sensor
+    light_val = photo_adc.read()
+
 
     # Show results on OLED
     oled.fill(0)
-    oled.text('R: {}'.format(r), 0, 8)
-    oled.text('G: {}'.format(g), 0, 16)
-    oled.text('B: {}'.format(b), 0, 24)
+    #oled.text('R: {}'.format(r), 0, 8)
+    #oled.text('G: {}'.format(g), 0, 16)
+    #oled.text('B: {}'.format(b), 0, 24)
+    oled.text('Light:', 0, 10)
+    oled.text(str(light_val), 0, 20)
     oled.show()
 
     # Print results
-    answer = '>r:{} g:{} b:{}<'.format(r, g, b)
-    print(answer, end='\n')
+    #answer = '>r:{} g:{} b:{}<'.format(r, g, b)
+    #print(answer, end='\n')
+    print("Phototransistor Reading: ", light_val)
 
     # Wait 1 second before repeating
     time.sleep(1)
+
